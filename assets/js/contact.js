8dd693e9d03f04d61c0b1f26efbc718db2279764
SachinRV18 <sachinrv1999@example.com>
2024-11-09 21:30:39 +0530

    updated the portfolio- 091124

---
+++ assets/js/contact.js
@@ -0,0 +1,31 @@
+// Initialize EmailJS
+emailjs.init("1gWKBPzwi1RJaWjz4"); // Replace with your EmailJS User ID
+
+// Get the form and submit event
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Show loading message
+    document.querySelector(".loading").style.display = "block";
+
+    // Send the email
+    emailjs.sendForm("service_gbjs4qu", "template_fldz9fo", this).then(
+      function (response) {
+        // Hide loading and show success message
+        document.querySelector(".loading").style.display = "none";
+        document.querySelector(".sent-message").style.display = "block";
+
+        // Redirect to the homepage after 3 seconds
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 3000);
+      },
+      function (error) {
+        // Hide loading and show error message
+        document.querySelector(".loading").style.display = "none";
+        document.querySelector(".error-message").style.display = "block";
+      }
+    );
+  });
